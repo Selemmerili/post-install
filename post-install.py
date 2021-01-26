12851e3ae18b727457bdd88f32e4cb63f214cf4a
Selemmerili <77363152+Selemmerili@users.noreply.github.com>
2021-01-26 16:35:48 +0100

    Update post-install.py

--- post-install.py
+++ post-install.py
@@ -1,23 +1,27 @@
 import os
 #!/bin/bash
 
-print("UBUNTU POST-INSTALL SCRIPT")
+print("INSTALLATION DU SCRIPT POST-INSTALL D'UBUNTU")
 
-print("Updating APT...")
+print("Lancement des maj")
 
 os.system("sudo apt-get update ")
 
 os.system("clear")
 
-print("Installing base packages")
+print("Installation des paquets de base ")
 
 os.system("sudo apt-get install --yes git git-extras build-essential python3-pip htop glances")
 
 os.system("clear")
 
+print("Installation de discord")
+
 os.system("sudo snap install discord")
 
 os.system("clear")
+
+print("Installation de Vscode")
 
 os.system("sudo snap install --classic code")
 
