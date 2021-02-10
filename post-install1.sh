d510b61233899f06a38b124d02361da6b878cce8
Selemmerili <77363152+Selemmerili@users.noreply.github.com>
2021-02-10 16:32:41 +0100

    Update post-install1.sh

--- post-install1.sh
+++ post-install1.sh
@@ -3,21 +3,22 @@
 echo "UBUNTU POST-INSTALL SCRIPT"
 
 echo "Updating APT..."
-
+#Commande pour installer les maj
 sudo apt-get update 
 
 clear
 
 echo "Installing base packages"
-
+#commande permettant d'installer python
 sudo apt-get install --yes git git-extras build-essential python3-pip htop glances
 
 clear
-
+#commande qui installe discord via snap
 sudo snap install discord
 echo "Installation de discord"
 clear
 
+#Installation de vscode via snap
 sudo snap install --classic code
 echo "Installation de vscode"
 
